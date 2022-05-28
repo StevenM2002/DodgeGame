56ad58b8600b4b5c29ba9393df61d683c27c7d84
Steven <stevenmai1111@hotmail.com>
2022-05-28 12:20:07 +1000

    minor changes

--- src/main/java/org/dodgegame/Board.java
+++ src/main/java/org/dodgegame/Board.java
@@ -1,11 +1,14 @@
 package org.dodgegame;
 
+import java.util.ArrayList;
+
 public class Board {
     public static int EMPTYSPACE = 0;
     public static int PLAYER = 1;
     public static int ENEMY = 2;
     public static int POINTOBJECTIVE = 3;
     public static int COLLISION = 4;
+    public static int OUTSIDEOFBOARD = -1;
     private int colLen;
     private int rowLen;
     private int numPoints = 0;
@@ -31,12 +34,12 @@ public class Board {
     }
 
     public void setEntityOnBoard(int col, int row, int spaceType) {
-        assert col < this.colLen && col >= 0;
-        assert row < this.rowLen && row >= 0;
+        if (!isInBoard(col, row)) throw new ArrayIndexOutOfBoundsException();
         this.theBoard[row][col] = spaceType;
     }
 
     public int getEntityOnBoard(int col, int row) {
+        if (!isInBoard(col, row)) return OUTSIDEOFBOARD;
         return this.theBoard[row][col];
     }
 
@@ -48,6 +51,45 @@ public class Board {
             System.out.println();
         }
         System.out.println();
+    }
+    public int[][] getTheBoard() {
+        return theBoard;
+    }
+    private void placeEntitiesOnBoard(ArrayList<ArrayList<Enemy>> allEnemyWave,
+                                      ArrayList<ArrayList<PointsObjective>> allPointsObjectives,
+                                      Player player) {
+        // waveLength is same for allPointsObjectives and allEnemyWave
+        int waveLength = allEnemyWave.size();
+        for (int i = 0; i < waveLength; i++) {
+            for (Enemy enemy : allEnemyWave.get(i)) {
+                this.setEntityOnBoard(enemy.getXLoc(), enemy.getYLoc(), Board.ENEMY);
+            }
+            for (PointsObjective point : allPointsObjectives.get(i)) {
+                this.setEntityOnBoard(point.getXLoc(), point.getYLoc(), Board.POINTOBJECTIVE);
+            }
+        }
+    }
+
+    public int numVertices() {
+        return colLen * rowLen;
+    }
+
+    public boolean isAdjacent(Coord a, Coord b) {
+        // If left or right
+        if (a.y() == b.y()) {
+            if (a.x() == b.x() + 1) return true;
+            if (a.x() == b.x() - 1) return true;
+        }
+        // If up or down
+        if (a.x() == b.x()) {
+            if (a.y() == b.y() + 1) return true;
+            if (a.y() == b.y() - 1) return true;
+        }
+        return false;
+    }
+
+    public boolean isInBoard(int x, int y) {
+        return x >= 0 && x < getXBoardLen() && y >= 0 && y < getYBoardLen();
     }
 
 }
